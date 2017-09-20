e86ddd97a726cde4cacd8fcb6b2544078e64edf5
bmihovski <bmihovski@boyan.mihovski.estafet.com>
2017-09-20 17:41:38 +0300

    POM and presentation updated

--- src/test/java/com/cucumber_training_bdd/checkout/step_definitions/CheckoutTest.java
+++ src/test/java/com/cucumber_training_bdd/checkout/step_definitions/CheckoutTest.java
@@ -3,10 +3,12 @@ package com.cucumber_training_bdd.checkout.step_definitions;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
  
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/com/cucumber_training_bdd/checkout/features/",
-		plugin = {"pretty", "html:target/cucumber"})
+		plugin = {"pretty", "html:target/cucumber"},
+		snippets=SnippetType.CAMELCASE, monochrome = true)
 public class CheckoutTest {
 }
